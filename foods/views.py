c8092007c760b32475d6cd362f0446a8eedc0775
hong-in-yeong <hongreat95@gmail.com>
2021-10-21 16:03:27 +0900

    기본 html로 변경

--- foods/views.py
+++ foods/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("<h2>Hello, Django !</h2>")
+    return render(request, 'foods/index.html')
