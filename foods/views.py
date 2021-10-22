bbfd437467ebe9f6348d25d8cfa8a917a4eb55fa
hong-in-yeong <hongreat95@gmail.com>
2021-10-22 20:55:44 +0900

    날짜정보 동적으로 입력

--- foods/views.py
+++ foods/views.py
@@ -1,6 +1,13 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+
 # Create your views here.
 def index(request):
-    return render(request, 'foods/index.html')
+    today = datetime.today().date()
+    # print(today)
+
+    context = {"date":today}
+
+    return render(request, 'foods/index.html',context=context)
