e084beed63091f65d91c9da3d882e535fbf8a744
hong-in-yeong <hongreat95@gmail.com>
2021-11-08 20:36:55 +0900

    url 셋팅 및 post의 list, detail 간 연결, url 연동 배움

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('posts/',views.post_list),
-    path('posts/<int:pk>',views.post_detail),
+    path('posts/',views.post_list, name='post-list'),
+    path('posts/<int:pk>',views.post_detail, name='post-detail'),
 ]
