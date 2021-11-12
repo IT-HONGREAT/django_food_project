59970f452421ba1ad3fee013ee6a10e9f2093b7b
hong-in-yeong <hongreat95@gmail.com>
2021-11-12 18:11:40 +0900

    forms 추가 및 html작성

--- posts/forms.py
+++ posts/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 from django.forms.widgets import Textarea
 
+
 class PostForm(forms.Form):
     title = forms.CharField(max_length=50, label='제목')
     content = forms.CharField(label='내용', widget=forms.Textarea)
+    feeling = forms.CharField(max_length=80, label='식후 상태')
+    score = forms.IntegerField(label='음식 점수')
+    dt_created = forms.DateField(label='날짜')
