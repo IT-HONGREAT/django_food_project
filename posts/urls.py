e3468f3b84ae235613e42869c1d5d61d1d233f23
hong-in-yeong <hongreat95@gmail.com>
2021-11-13 20:32:19 +0900

    post개념 공부 및 업로드 시 발생하는 문제 확인;파라미터 값 혼동으로 인함. 덕분에 views,urls 공부가 되었음.(주의하자)

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('posts/', views.post_list, name='post-list'),
-    path('posts/<int:pk>', views.post_detail, name='post-detail'),
+    path('posts/<int:post_id>', views.post_detail, name='post-detail'),
     path('posts/create', views.post_create, name='post-create'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from posts.forms import PostForm
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Post
 # Create your views here.
 
@@ -12,15 +12,27 @@ def post_list(request):
     return render(request, 'posts/post_list.html', context)
 
 
-def post_detail(request, pk):
+def post_detail(request, post_id):
 
     context = dict()
-    post = Post.objects.get(id=pk)
+    post = Post.objects.get(id=post_id)
     context = {"post": post}
     return render(request, 'posts/post_detail.html', context)
 
 
 def post_create(request):
+    # request를 통해서 들어온 것을 post인지 구분하게 해야함
+    if request.method == 'POST':
+        title = request.POST['title'],
+        content = request.POST['content'],
+        new_post = Post(
+            title=title,
+            content=content,
+        )
+        new_post.save()
+        return redirect('post-detail', post_id=new_post.id)
 
-    post_form = PostForm
-    return render(request, 'posts/post_form.html', {'form': post_form})
+    # get방식 일 때, 폼을 돌려주는 원래 페이지로 줌
+    else:
+        post_form = PostForm
+        return render(request, 'posts/post_form.html', {'form': post_form})
