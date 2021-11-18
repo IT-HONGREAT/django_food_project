ed4bbc3da4e420ab52b705bf355372a8f35e5d02
hong-in-yeong <hongreat95@gmail.com>
2021-11-19 02:05:29 +0900

    form placeholder 적용

--- posts/forms.py
+++ posts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.forms.widgets import Textarea
+from django.forms import widgets
+from django.forms.widgets import Textarea, Widget
 from .models import Post
 from .validators import validate_numbers
 from django.core.exceptions import ValidationError
@@ -18,6 +19,10 @@ class PostForm(forms.ModelForm):  # 장고제공_모델폼으로 수정( 위의 
         model = Post
         fields = ['title', 'food_img', 'feeling',
                   'score', 'content']  # 선택 출력 (date는 안불러와짐)
+        widgets = {'title': forms.TextInput(
+            attrs={'class': 'title', 'placeholder': '제목을 입력하세요'}),
+            'content': forms.Textarea(
+            attrs={'placeholder': '내용을 입력하세요'})}
         # fields = '__all__'  # 전체 출력
 
     def clean_title(self):
