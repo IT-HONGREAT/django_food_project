af4c5d60bbb57e845b1ad54ac4f9b8b6c0b7583e
hong-in-yeong <hongreat95@gmail.com>
2021-11-21 18:49:36 +0900

    모듈 생성; 글의 수정일이 생성일 보다 먼저인 에러를 잡기위함. post의 id와 title 로 확인가능, shell 에서 실행하면 됨.

---
+++ posts/validate_data.py
@@ -0,0 +1,18 @@
+from .models import Post
+
+posts = Post.objects.all()
+
+
+def validate_post():
+
+    for post in posts:
+        if '&' in post.content:
+            print(f"{post.id}번글에 & 가 있습니다.")
+
+            post.content = post.content.replace('&', '')
+
+            post.save()
+        if post.created_date > post.modified_date:
+
+            print(f"{post.id}번글(제목:{post.title})의 생성일이 수정일보다 큽니다.")
+            post.save()
