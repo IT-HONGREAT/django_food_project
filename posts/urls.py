eb9c90433dfd73c2a840aa7d121cc21b28cc4062
hong-in-yeong <hongreat95@gmail.com>
2021-11-26 00:22:52 +0900

    real generic화함, 코드는 간결하나 구조는 파악하기 애매함.그래도 좋은 기능이다.

--- posts/urls.py
+++ posts/urls.py
@@ -3,12 +3,12 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexRedirectView.as_view(), name='index'),
     path('posts/', views.PostListView.as_view(), name='post-list'),
-    path('posts/<int:post_id>', views.PostDetailView.as_view(), name='post-detail'),
+    path('posts/<int:pk>', views.PostDetailView.as_view(), name='post-detail'),
     path('posts/create', views.PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:post_id>/edit',
+    path('posts/<int:pk>/edit',
          views.PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:post_id>/delete',
+    path('posts/<int:pk>/delete',
          views.PostDeleteView.as_view(), name='post-delete'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -3,69 +3,58 @@ from django.db.models.base import Model
 from django.shortcuts import redirect, render, get_object_or_404
 from django.http import Http404
 from django.core.paginator import Paginator
-from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView, RedirectView
 from django.urls import reverse
 from posts.forms import PostForm
 from .models import Post
 # Create your views here.
 
 
+# def index(request):
+#     return redirect('post-list')
+class IndexRedirectView(RedirectView):
+    pattern_name = 'post-list'
+
+
 class PostListView(ListView):
 
     model = Post
-    template_name = 'posts/post_list.html'
-    context_object_name = 'posts'
     ordering = ['-created_date']
     paginate_by = 6
-    page_kwarg = 'page'
 
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'posts/post_detail.html'
-    pk_url_kwarg = 'post_id'
-    context_object_name = 'post'
+    # template_name = 'posts/post_detail.html'  # 모델명_각뷰이름.html이 기본값, 자동 적용
+    # pk_url_kwarg = 'post_id'         # url 에서 pk가 기본 값이기 때문에 변경해주면 삭제해도됨
+    # context_object_name = 'post'   # 제너릭의 기본 값 모델을 소문자로 쓰면됨
 
 
 class PostCreateView(CreateView):
     model = Post
     form_class = PostForm
-    template_name = 'posts/post_form.html'
+    # template_name = 'posts/post_form.html'
 
     def get_success_url(self):
-        return reverse('post-detail', kwargs={'post_id': self.object.id})
+        return reverse('post-detail', kwargs={'pk': self.object.id})
 
 
 class PostUpdateView(UpdateView):
     model = Post
     form_class = PostForm
-    template_name = 'posts/post_form.html'
-    pk_url_kwarg = 'post_id'
+    # template_name = 'posts/post_form.html'
+    # pk_url_kwarg = 'post_id'
 
     def get_success_url(self):
-        return reverse('post-detail', kwargs={'post_id': self.object.id})
-
-
-def post_delete(request, post_id):
-
-    post = get_object_or_404(Post, id=post_id)
-    if request.method == 'POST':
-        post.delete()
-        return redirect('post-list')
-    else:
-        return render(request, 'posts/post_delete_confirm.html', {'post': post})
+        return reverse('post-detail', kwargs={'pk': self.object.id})
 
 
 class PostDeleteView(DeleteView):
 
     model = Post
-    template_name = 'posts/post_delete_confirm.html'
-    pk_url_kwarg = 'post_id'
-    context_object_name = 'post'
+    # template_name = 'posts/post_delete_confirm.html'
+    # pk_url_kwarg = 'post_id'
+    # context_object_name = 'post'
 
     def get_success_url(self):
         return reverse('post-list')
-
-
-def index(request):
-    return redirect('post-list')
