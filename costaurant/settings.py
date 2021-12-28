96a36c9cef18ee101c9eb1a67dccf80b60fd21c7
hong-in-yeong <hongreat95@gmail.com>
2021-12-29 01:01:00 +0900

    [#17] middleware를 활용하여 본 issue를 해결, middleware에 대해서 알게되는 좋은 결과가 됨.

--- costaurant/settings.py
+++ costaurant/settings.py
@@ -75,6 +75,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'foods.middleware.ProfileSetupMiddleware',
 ]
 
 ROOT_URLCONF = 'costaurant.urls'
